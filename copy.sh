12684808209d25ab91be66f1d8b5c3adf78c25bf
wo2ni <i_o_i_o@163.com>
2024-02-12 20:40:09 +0800

    :rocket: docker shell

---
+++ copy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+cp -vr config/    WEB 
+cp -vr db/     WEB
+cp -vr extra/      WEB
+cp -vr node_modules/    WEB
+cp -v package-lock.json     WEB
+cp -vr server/      WEB
+cp -vr data/       WEB
+cp -vr dist/     WEB
+cp -v index.html      WEB
+cp -v package.json     WEB
+cp -vr public/    WEB
+cp -vr src/     WEB
